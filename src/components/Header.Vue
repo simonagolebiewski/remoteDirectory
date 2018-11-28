baad51f266e5db32a88d26d647fd5c17836576cc
Maciej Golebiewski <maciejmg12@gmail.com>
2018-11-27 21:33:15 -0500

    meta info and image

--- src/components/Header.Vue
+++ src/components/Header.Vue
@@ -3,10 +3,7 @@
   <div class="header-container__border-top"></div>
   <div class="header-container__share-links">
     <div class="header-container__share-links__share">> Share</div>
-    <a href="#"><div class="header-container__share-links__icon"></div></a>
-    <a href="#"><div class="header-container__share-links__icon"></div></a>
-    <a href="#"><div class="header-container__share-links__icon"></div></a>
-    <a href="#" class="header-container__share-links__contribute">+ Contribute</a>
+    <a href="https://twitter.com/intent/tweet?url=http%3A%2F%2Fhiringremote.ly&text=Looking%20for%20a%20remote%20job%3F%20This%20site%20let's%20you%20efficiently%20find%20remote%20companies%20that%20are%20uniquely%20suited%20to%20you%20and%20your%20needs."><div class="header-container__share-links__icon">Tweet about us</div></a>
   </div>
 </div>
 </template>
@@ -41,10 +38,17 @@ export default {
     }
     &__icon {
       height: 22px;
-      width: 22px;
-      border-radius: 100%;
-      background: #FFFFFF;
+      width: 122px;
+      border-radius: 4px;
+      background: #F4F6F8;
+      text-align: center;
+      line-height: 22px;
       box-shadow: 0 2px 3px -2px rgba(24,24,24,0.20), 0 0 0 1px rgba(24,24,24,0.10);
+      color: #1DA1F2;
+      &:hover {
+        background: #fff;
+        text-decoration: underline;
+      }
     }
     &__contribute {
       margin-left: 8px;
@@ -56,7 +60,6 @@ export default {
       display: inline-block;
       font-family: 'Roboto-Bold', sans-serif;
       font-size: 14px;
-      color: #24292E;
       vertical-align: middle;
     }
   }
